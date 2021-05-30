4eb0b263a94de9dfd0283b47f1594ea7b9161ce8
mrbald <mrbald@users.noreply.github.com>
2021-05-30 18:50:59 +0200

    pipeline.h tech refresh

--- ringbuf/pipeline.h
+++ ringbuf/pipeline.h
@@ -19,131 +19,153 @@
 #include <atomic>
 #include <type_traits>
 
-#ifndef UFW_L1D_LINE_SIZE
-#   error "macro UFW_L1D_LINE_SIZE not defined"
-#endif
+#include <immintrin.h>
 
 namespace ufw {
 
+#if __x86_64__
+inline
+#endif
+namespace x86_64 {
+inline void zzz() noexcept { _mm_pause(); }
+}
+
 namespace details {
+
+inline constexpr size_t l1d_line_size = 64u;
 
 template <class T, class S, bool CallCtor, bool CallDtor> struct lifecycle_tracker;
 
 template <class T, class S> struct lifecycle_tracker<T, S, false, false> {
-    lifecycle_tracker(S&) {}
+  explicit lifecycle_tracker(S&) {}
 };
 
 template <class T, class S> struct lifecycle_tracker<T, S, true, false> {
-    lifecycle_tracker(S& node) { new (&node) T; }
+  explicit lifecycle_tracker(S& node) { new (&node) T; }
 };
 
 template <class T, class S> struct lifecycle_tracker<T, S, false, true> {
-    S& node_;
-    lifecycle_tracker(S& node): node_(node) {}
-    ~lifecycle_tracker() { reinterpret_cast<T&>(node_).~T(); }
+  S& node_;
+  explicit lifecycle_tracker(S& node): node_(node) {}
+  ~lifecycle_tracker() { reinterpret_cast<T&>(node_).~T(); }
 };
 
 } // namespace details
 
 /**
+ A symmetric single-actor-per-station pipeline.
+
  [(X-1)%N] <<== can consume from == [X%N] == can produce for ==>> [(X+1)%N]
-*/
-template <class T, size_t C, size_t N, size_t L = 0> struct pipeline {
-    static_assert(C <= std::numeric_limits<size_t>::max() && N >= 2 && L < N, "");
-    using value_type = T;
-    static constexpr auto CAP = C;
-    static constexpr auto STG = N;
-    static constexpr auto FIRST_STAGE_ID = L;
-    static constexpr auto LAST_STAGE_ID = (L - 1) + (STG * !L);
 
-    using node_t = std::aligned_storage_t<sizeof(T), alignof(T)>;
+ * @tparam T data type
+ * @tparam C ring buffer capacity
+ * @tparam N number of stages (default = 2, an spsc queue)
+ * @tparam L first stage IDx (default = 0)
+ */
+template <class T, size_t C, size_t N = 2, size_t L = 0> struct pipeline {
+  static_assert(C <= std::numeric_limits<size_t>::max() && N >= 2 && L < N);
 
-private:
-    static constexpr size_t CAUGHT_UP_BIT = 1ull << 63;
-    struct stage { alignas(UFW_L1D_LINE_SIZE) std::atomic<size_t> pos_ {CAUGHT_UP_BIT}; };
+  using value_type = T;
+  static constexpr auto CAP = C;
+  static constexpr auto STG = N;
+  static constexpr auto FIRST_STAGE_ID = L;
+  static constexpr auto LAST_STAGE_ID = (L - 1) + (STG * !L);
 
-    std::array<stage, STG> stages_;
-    std::array<node_t, CAP> nodes_;
+  using node_t = std::aligned_storage_t<sizeof(T), alignof(T)>;
 
-    size_t static mod_cap(size_t x) noexcept { return x - C * (x >= C); }
+ private:
+  static constexpr size_t CAUGHT_UP_BIT = 1ull << 63u;
+  struct stage { alignas(details::l1d_line_size) std::atomic<size_t> pos_ {CAUGHT_UP_BIT}; };
 
-public:
+  std::array<stage, STG> stages_;
+  std::array<node_t, CAP> nodes_;
 
-    pipeline() noexcept {
-        stages_[LAST_STAGE_ID].pos_ ^= CAUGHT_UP_BIT;
+  size_t static mod_cap(size_t x) noexcept { return x - C * (x >= C); }
+
+ public:
+
+  pipeline() noexcept {
+    stages_[LAST_STAGE_ID].pos_ ^= CAUGHT_UP_BIT;
+  }
+
+  /**
+   * Callback signature: void(node_t*, size_t len, Args...)
+   * "V" is for ???
+   */
+  template <size_t X, size_t BATCH_SIZE = CAP, class Func, class... Args>
+  size_t invokev(Func&& func, Args&&... args) noexcept {
+    static_assert(X >= 0 && X < STG && BATCH_SIZE <= CAP);
+    static_assert(std::is_nothrow_invocable_v<Func, node_t*, size_t, Args...>);
+
+    if (!BATCH_SIZE) return 0;
+
+    auto& cur_stage_pos_ = stages_[X].pos_;
+    auto& prev_stage_pos_ = stages_[(X - 1) + (STG * !X)].pos_;
+
+    auto const prev_stage_pos_masked = prev_stage_pos_.load(std::memory_order_acquire);
+    if (prev_stage_pos_masked & CAUGHT_UP_BIT)
+      return 0;
+
+    auto const cur_stage_pos_masked = cur_stage_pos_.load(std::memory_order_acquire);
+    auto const cur_stage_pos = cur_stage_pos_masked & ~CAUGHT_UP_BIT;
+
+    auto prev_stage_pos = prev_stage_pos_masked & ~CAUGHT_UP_BIT;
+
+    size_t const batch_size_possible = prev_stage_pos - cur_stage_pos + CAP * (prev_stage_pos <= cur_stage_pos);
+    size_t const batch_size = std::min(BATCH_SIZE, batch_size_possible);
+
+    if (cur_stage_pos + batch_size > CAP) {
+      func(&nodes_[cur_stage_pos], CAP - cur_stage_pos, std::forward<Args>(args)...);
+      func(&nodes_[0], batch_size - (CAP - cur_stage_pos), std::forward<Args>(args)...);
+    } else {
+      func(&nodes_[cur_stage_pos], batch_size, std::forward<Args>(args)...);
     }
 
-    /**
-     * Callback signature: void(node_t*, size_t len, Args...)
-     */
-    template <size_t X, size_t BATCH_SIZE = CAP, class Func, class... Args>
-    size_t invokev(Func&& func, Args&&... args) noexcept {
-        static_assert(X >= 0 && X < STG && BATCH_SIZE <= CAP, "");
+    // raise caught-up bit if the previous stage has not made the progress
+    // and the current stage has consumed all values
+    if (batch_size == batch_size_possible)
+      prev_stage_pos_.compare_exchange_strong(prev_stage_pos,
+                                              prev_stage_pos | CAUGHT_UP_BIT,
+                                              std::memory_order_acq_rel, std::memory_order_relaxed);
 
-        if (!BATCH_SIZE) return 0;
+    // save unmasked value to release the consumer
+    cur_stage_pos_.store(mod_cap(cur_stage_pos + batch_size), std::memory_order_release);
 
-        auto& cur_stage_pos_ = stages_[X].pos_;
-        auto& prev_stage_pos_ = stages_[(X - 1) + (STG * !X)].pos_;
-
-        auto const prev_stage_pos_masked = prev_stage_pos_.load(std::memory_order_acquire);
-        if (prev_stage_pos_masked & CAUGHT_UP_BIT)
-            return 0;
-
-        auto const cur_stage_pos_masked = cur_stage_pos_.load(std::memory_order_acquire);
-        auto const cur_stage_pos = cur_stage_pos_masked & ~CAUGHT_UP_BIT;
-
-        auto prev_stage_pos = prev_stage_pos_masked & ~CAUGHT_UP_BIT;
-
-        size_t const batch_size_possible = prev_stage_pos - cur_stage_pos + CAP * (prev_stage_pos <= cur_stage_pos);
-        size_t const batch_size = std::min(BATCH_SIZE, batch_size_possible);
-
-        if (cur_stage_pos + batch_size > CAP) {
-            func(&nodes_[cur_stage_pos], CAP - cur_stage_pos, std::forward<Args>(args)...);
-            func(&nodes_[0], batch_size - (CAP - cur_stage_pos), std::forward<Args>(args)...);
-        } else {
-            func(&nodes_[cur_stage_pos], batch_size, std::forward<Args>(args)...);
-        }
-
-        // raise caught-up bit if the previous stage has not made the progress
-        // and the current stage has consumed all values
-        if (batch_size == batch_size_possible)
-            prev_stage_pos_.compare_exchange_strong(prev_stage_pos,
-                    prev_stage_pos | CAUGHT_UP_BIT,
-                    std::memory_order_acq_rel, std::memory_order_relaxed);
-
-        // save unmasked value to release the consumer
-        cur_stage_pos_.store(mod_cap(cur_stage_pos + batch_size), std::memory_order_release);
-
-        return batch_size;
-    }
+    return batch_size;
+  }
 
 
-    /**
-     * Callback signature: void(node_t&, Args...)
-     */
-    template <size_t X, size_t BATCH_SIZE = CAP, class Func, class... Args>
-    size_t invokem(Func&& func, Args&&... args) noexcept {
-        return invokev<X, BATCH_SIZE>([&func] (node_t* beg, size_t len, Args&&... args) noexcept -> decltype(auto) {
-            for (auto* ptr = beg, end = beg + len; ptr < end; ++ptr)
-                func(*ptr, std::forward<Args>(args)...);
-        }, std::forward<Args>(args)...);
-    }
+  /**
+   * Callback signature: void(node_t&, Args...)
+   * "M" is for "multi"
+   */
+  template <size_t X, size_t BATCH_SIZE = CAP, class Func, class... Args>
+  size_t invokem(Func&& func, Args&&... args) noexcept {
+    static_assert(std::is_nothrow_invocable_v<Func, node_t&, Args...>);
+
+    return invokev<X, BATCH_SIZE>([&func] (node_t* beg, size_t len, Args&&... args) noexcept -> decltype(auto) {
+      for (auto *ptr = beg, *end = beg + len; ptr < end; ++ptr)
+        func(*ptr, std::forward<Args>(args)...);
+    }, std::forward<Args>(args)...);
+  }
 
 
-    /**
-     * Callback signature: void (T&, Args...)
-     * Automatically constructs the T before the first stage invocation
-     * and destructs after the last stage invocation
-     */
-    template <size_t X, size_t n = C, class Func, class... Args>
-    size_t invoke(Func&& func, Args&&... args) noexcept {
-        return invokem<X, n>([&func] (node_t& node, Args&&... args) noexcept -> decltype(auto) {
-            details::lifecycle_tracker<T, node_t,
-                    X == FIRST_STAGE_ID && !std::is_trivially_constructible<T>::value,
-                    X == LAST_STAGE_ID && !std::is_trivially_destructible<T>::value> _(node);
-            return func(reinterpret_cast<T&>(node), std::forward<Args>(args)...);
-        }, std::forward<Args>(args)...);
-    }
+  /**
+   * Callback signature: void (T&, Args...)
+   * Automatically constructs the T before the first stage invocation
+   * and destructs after the last stage invocation
+   */
+  template <size_t X, size_t n = C, class Func, class... Args>
+  size_t invoke(Func&& func, Args&&... args) noexcept {
+    static_assert(std::is_nothrow_invocable_v<Func, T&, Args...>);
+
+    return invokem<X, n>([&func] (node_t& node, Args&&... args) noexcept -> decltype(auto) {
+      details::lifecycle_tracker<T, node_t,
+          X == FIRST_STAGE_ID && !std::is_trivially_constructible<T>::value,
+          X == LAST_STAGE_ID && !std::is_trivially_destructible<T>::value> _(node);
+      return func(reinterpret_cast<T&>(node), std::forward<Args>(args)...);
+    }, std::forward<Args>(args)...);
+  }
 
 };
 
